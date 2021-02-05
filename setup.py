e2fba52fa149c513c0f40e1cb0370c700ac01bcd
James Murphy <james@mcoding.io>
2021-02-05 16:07:03 -0600

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mCodingLLC/prime_sieve',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
