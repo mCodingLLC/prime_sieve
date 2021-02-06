b40934fc1f789500b2e1fd80e0500236520c5956
James Murphy <james@mcoding.io>
2021-02-06 10:56:41 -0600

    Bump version: 0.1.9 → 0.1.10

--- prime_sieve/__init__.py
+++ prime_sieve/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """James Murphy"""
 __email__ = 'james@mcoding.io'
-__version__ = '0.1.9'
+__version__ = '0.1.10'
--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mCodingLLC/prime_sieve',
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
